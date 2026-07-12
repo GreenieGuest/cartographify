5003869145c285b329433311c9fd3b91ca34a2eb
GreenieGuest <papootreasure@yahoo.com>
2026-07-12 00:20:50 -0500

    add old map modes

--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -1,5 +1,26 @@
 export default function Toolbar() {
 
+    const VIEWS = [
+        { id: 'default', label: 'Default (Location)'},
+        { id: 'assigned', label: 'Assigned'},
+        { id: 'tradeGood', label: 'Trade Goods'},
+        { id: 'population', label: 'Population'},
+        { id: 'vegetation', label: 'Vegetation'},
+        { id: 'terrain', label: 'Topography'},
+        { id: 'climate', label: 'Climate'},
+        { id: 'owner', label: 'Owner'},
+        { id: 'culture', label: 'Culture'},
+        { id: 'religion', label: 'Religion'},
+        { id: 'harbors', label: 'Harbors'},
+        //Hierarchical
+        { id: 'continent', label: 'Continent'},
+        { id: 'subcontinent', label: 'Subcontinent'},
+        { id: 'region', label: 'Region'},
+        { id: 'area', label: 'Area'},
+        { id: 'province', label: 'Province'},
+        { id: 'isCoastal', label: 'Coastal'},
+    ]
+
     return (
         <aside class="left-sidebar">
           <h3>Tools & Data</h3>
---
+++ src/constants/climatecolors.js
@@ -0,0 +1,11 @@
+const CLIMATE_COLORS = {
+    arctic:[153, 206, 203],
+    continental:[68, 158, 77],
+    mediterranean:[198, 255, 78],
+    tropical:[14, 58, 9],
+    subtropical:[18, 98, 34],
+    arid:[167, 117, 58],
+    cold_arid:[181, 172, 118],
+    oceanic:[102, 255, 51],
+}
+export default CLIMATE_COLORS;
