877eab754f604edf52c5ed6f30ab879e3a0363e6
GreenieGuest <papootreasure@yahoo.com>
2026-07-11 19:31:26 -0500

    feat: implement main layout

--- src/App.jsx
+++ src/App.jsx
@@ -11,15 +11,17 @@ function App() {
         <p>visualize your map for eu5 or for other purposes</p>
         <p>made by GreenieGuest</p>
       </header>
-      <aside class="left-sidebar">
-        <h3>Tools & Data</h3>
-      </aside>
-      <main>
-        <p>Map will go here</p>
-      </main>
-      <aside class="right-sidebar">
-        <h3>Tools & Data</h3>
-      </aside>
+      <div class="content-wrapper">
+        <aside class="left-sidebar">
+          <h3>Tools & Data</h3>
+        </aside>
+        <main>
+          <p>Map will go here</p>
+        </main>
+        <aside class="right-sidebar">
+          <h3>Tools & Data</h3>
+        </aside>
+      </div>
     </>
   )
 }
