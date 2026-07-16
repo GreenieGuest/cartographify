948e8792d25285b0eedc8f424b21ed8ab70384a4
GreenieGuest <papootreasure@yahoo.com>
2026-07-16 03:07:14 -0500

    change pan/zoom to refs

--- src/components/MapDisplay.jsx
+++ src/components/MapDisplay.jsx
@@ -7,11 +7,11 @@ export default function MapDisplay() {
     const { mapImage } = useMapStore()
 
     // pan/zoom variables
-    const [pan, setPan] = useState({ x: 0, y: 0 })
-    const [zoom, setZoom] = useState(1)
+    const pan = useRef({ x: 0, y: 0 })
+    const zoom = useRef(1)
     const zoomIntensity = useRef(0.1)
     const isPanning = useRef(false)
-    const [lastMouse, setLastMouse] = useState({ x: 0, y: 0 })
+    const lastMouse = useRef({ x: 0, y: 0 })
 
     // Draw function for canvas (call each time pan/zoom/window resizes)
     const draw = (e) => {
@@ -19,13 +19,15 @@ export default function MapDisplay() {
         const canvas = mapcanvas.current;
         const ctx = canvas.getContext('2d');
         ctx.save()
+        ctx.imageSmoothingEnabled = false;
         ctx.setTransform(1, 0, 0, 1, 0, 0)
         ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-        ctx.translate(pan.x, pan.y)
-        ctx.scale(zoom, zoom)
+        ctx.translate(pan.current.x, pan.current.y)
+        ctx.scale(zoom.current, zoom.current)
 
         ctx.drawImage(mapImage, 0, 0);
+        console.log(pan.current, zoom.current)
         ctx.restore()
     }
 
@@ -47,40 +49,39 @@ export default function MapDisplay() {
     // Event listeners for pan/zoom
     const handleMouseDown = (e) => {
         isPanning.current = true;
-        setLastMouse({
-            x: e.clientX - pan.x,
-            y: e.clientY - pan.y
-        })
+        lastMouse.current = {
+            x: e.clientX - pan.current.x,
+            y: e.clientY - pan.current.y
+        }
     }
 
     const handleMouseUp = (e) => { isPanning.current = false }
 
     const handleMouseMove = (e) => {
         if (!isPanning.current) return;
-        setPan({
-            x: e.clientX - lastMouse.x,
-            y: e.clientY - lastMouse.y
-        })
+        pan.current = {
+            x: e.clientX - lastMouse.current.x,
+            y: e.clientY - lastMouse.current.y
+        }
+        draw();
     }
     const handleMouseWheel = (e) => {
         e.preventDefault()
-        const rect = mapcanvas.current.getBoundingClientRect();
+        const { rx, ry } = getRectXY(e);
 
-        const mouseX = e.clientX - rect.left;
-        const mouseY = e.clientY - rect.top;
-
-        let newZoom = e.deltaY < 0 ? zoom + zoomIntensity.current : zoom - zoomIntensity.current;
-        newZoom = Math.min(Math.max(.5, newZoom), 5);
+        let newZoom = e.deltaY < 0 ? zoom.current + zoomIntensity.current : zoom.current - zoomIntensity.current;
+        newZoom = Math.min(Math.max(.1, newZoom), 50);
 
         // zoom towards cursor position
 
-        const scale = newZoom / zoom;
+        const scale = newZoom / zoom.current;
 
-        setPan({
-            x: mouseX - (mouseX - pan.x) * scale,
-            y: mouseY - (mouseY - pan.y) * scale
-        });
-        setZoom(newZoom);
+        pan.current = {
+            x: rx - (rx - pan.current.x) * scale,
+            y: ry - (ry - pan.current.y) * scale
+        };
+        zoom.current = newZoom;
+        draw();
     }
     
     return (
